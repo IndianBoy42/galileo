791673a26a77d1c46a83a67edc43fc6a8039daaa
Anshuman <amedhi@connect.ust.hk>
2024-10-07 18:51:01 +0800

    fix texture.present()

--- galileo/src/render/wgpu/mod.rs
+++ galileo/src/render/wgpu/mod.rs
@@ -627,17 +627,15 @@ impl WgpuRenderer {
         self.render_map(map, view);
     }
 
-    /// Renders the map.
+    /// Renders the map. `render_texture` and always present
     pub fn render(&self, map: &Map) -> Result<(), SurfaceError> {
-        self.render_texture(map)?;
-
-        texture.present();
+        self.render_texture(map, true)?;
 
         Ok(())
     }
 
-    /// Renders the map but doesn't present.
-    pub fn render_texture(&self, map: &Map) -> Result<(), SurfaceError> {
+    /// Renders the map and optionally present
+    pub fn render_texture(&self, map: &Map, present: bool) -> Result<(), SurfaceError> {
         let Some(render_set) = &self.render_set else {
             return Ok(());
         };
@@ -646,6 +644,10 @@ impl WgpuRenderer {
         let view = texture.view();
 
         self.render_to_texture_view(map, &view);
+
+        if present {
+            texture.present();
+        }
 
         Ok(())
     }
